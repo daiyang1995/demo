a40862a53211e62b7b55bb09828a785d02ccc43c
pater <505955007@qq.com>
2017-07-12 08:40:36 +0800

    例子

---
+++ 检测8080 8005 8009.bat
@@ -0,0 +1,4 @@
+netstat -ano|findstr 8005
+netstat -ano|findstr 8009
+netstat -ano|findstr 8080
+pause
---
+++ 检测8180,8105,8109.bat
@@ -0,0 +1,6 @@
+netstat -ano|findstr 8105
+netstat -ano|findstr 8109
+netstat -ano|findstr 8180
+pause
+taskkill /f /t /pid 
+pause
